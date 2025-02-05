2f5864b94bebce8f7701ca2f142cbed5029a58f9
MameHub <acaygo@gmail.com>
2025-02-05 18:14:03 +0100

    toggle y holi

--- app/page.js
+++ app/page.js
@@ -5,24 +5,34 @@ import { useState } from "react";
 export default function Componentes() {
   return (
     <>
-      <Toggle/>
-      {/* <Holi/>
-      <Destino/> */}
+      <div><Toggle/></div>
+      <div><br/></div>
+      <Holi nombre="Álvaro" />
+      <Holi nombre="" />
+      <div><br/></div>
+      {/* <Destino/> */}
     </>
   );
 }
 
-export function Toggle() {
-  return (
-    <button className="btn" type="button">off</button>
-  );
+export const Toggle = () => {
+  const[on, setOn] = useState(false);
+
+  return(
+    <button className={on?"btnOn":"btnOff"} onClick = {() => setOn(!on)}>
+      {on ? "on" : "off"}
+    </button>
+  )
 }
 
-// export function Holi() {
-//   return (
-    
-//   );
-// }
+export function Holi({nombre}) {
+  return (
+    <div>
+      {nombre && <p>Hola {nombre}, como estas?</p>}
+      {!nombre && <p>Hola. No te conozco</p>}
+    </div>
+  );
+}
 
 // export function Destino() {
 //   return (
